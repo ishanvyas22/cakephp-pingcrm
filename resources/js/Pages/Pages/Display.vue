82f1cb99990c0c9052a47d638f9f6016a96e1158
Ishan Vyas <isvyas@gmail.com>
2020-08-30 19:37:36 +0530

    Add example Display.vue file

---
+++ resources/js/Pages/Pages/Display.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Hello! This is display page.
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Display.vue component mounted.');
+    }
+}
+</script>
--- src/View/AppView.php
+++ src/View/AppView.php
@@ -17,10 +17,6 @@ class AppView extends View
     /**
      * Initialization hook method.
      *
-     * Use this method to add common initialization code like loading helpers.
-     *
-     * e.g. `$this->loadHelper('Html');`
-     *
      * @return void
      */
     public function initialize()
