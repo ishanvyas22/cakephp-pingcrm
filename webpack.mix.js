4553c46297f40025a65a53c5aece24a8cd4d3e2f
Ishan Vyas <isvyas@gmail.com>
2020-08-30 19:07:06 +0530

    Setup tailwindcss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,32 @@
+const cssImport = require('postcss-import');
+const cssNesting = require('postcss-nesting');
 const mix = require('laravel-mix');
 const path = require('path');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const tailwindcss = require('tailwindcss');
 
 mix.setPublicPath('./webroot')
     .js('resources/js/app.js', 'webroot/js')
-    .sass('resources/sass/app.scss', 'webroot/css')
+    .postCss('resources/css/app.css', 'public/css/app.css')
+    .options({
+        postCss: [
+            cssImport(),
+            cssNesting(),
+            tailwindcss('tailwind.config.js'),
+            ...mix.inProduction() ? [
+                purgecss({
+                    content: ['./src/Template/**/*.ctp', './resources/js/**/*.vue'],
+                    defaultExtractor: content => content.match(/[\w-/:.]+(?<!:)/g) || [],
+                    whitelistPatternsChildren: [/nprogress/],
+                }),
+            ] : [],
+        ],
+    })
     .webpackConfig({
-        output: { chunkFilename: 'js/[name].js?id=[chunkhash]' },
-            resolve: {
+        output: {
+            chunkFilename: 'js/[name].js?id=[chunkhash]'
+        },
+        resolve: {
             alias: {
                 vue$: 'vue/dist/vue.runtime.esm.js',
                 '@': path.resolve('resources/js'),
