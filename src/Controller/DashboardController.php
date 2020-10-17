e3016b7a5e8607296ee31f76c0be45923748b7ea
Ishan Vyas <isvyas@gmail.com>
2020-10-17 21:29:09 +0530

    Bake dashboard controller

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Dashboard Controller
+ */
+class DashboardController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function index()
+    {
+    }
+}
